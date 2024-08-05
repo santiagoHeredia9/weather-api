648f087a9664d92b0b22b8a321aa4597f9952879
santiagoHeredia <santiagoherediabrandolini@gmail.com>
2024-08-05 19:50:25 -0300

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = 10000;
 
 app.get("/api/weather", (req, res) => {
   fs.readFile(path.join(__dirname, "data", "db.json"), "utf-8", (err, data) => {
